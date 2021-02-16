b67e1c2b119f78af3cb875b7aed4b3c56ff864a1
worker <worker@adm-group.ru>
2021-02-16 16:07:26 +0300

    fix bit price

--- Bitcoin_price/price_BIT.py
+++ Bitcoin_price/price_BIT.py
@@ -1,17 +1,41 @@
-import requests
+# import requests
+from requests import Session, TooManyRedirects,Timeout
+import json
 
-URL = 'http://api.coinmarketcap.com/v1/ticker/'  # на 16 февраля 2021 данная ссылка работает
-COIN = "bitcoin"
-last_price = None
+# URL = 'http://api.coinmarketcap.com/v1/ticker/'  # на 16 февраля 2021 данная ссылка не работает
+# COIN = "bitcoin"
+# last_price = None
 
 
-def scrape():
-    response = requests.get(URL+COIN)
-    response_json = response.json()
-    return float(response_json[0]['price_usd']) 
+# def scrape():
+#     response = requests.get(URL+COIN)
+#     response_json = response.json()
+#     return float(response_json[0]['USD']) 
 
-while True:
-    latest_price = scrape()
-    if latest_price != last_price:
-        print("", latest_price)
-        last_price = latest_price
+# while True:
+#     latest_price = scrape()
+#     if latest_price != last_price:
+#         print("Последняя цена: ", latest_price)
+#         last_price = latest_price
+
+
+url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest'
+parameters = {
+  'start':'1',
+  'limit':'5000',
+  'convert':'USD'
+}
+headers = {
+  'Accepts': 'application/json',
+  'X-CMC_PRO_API_KEY': 'Здесь должен быть Ваш Api-key',  # Получить его можно 
+}                                       #   зарегнистрировавшысь на coinmarketcap, в личном кабинете
+
+session = Session()
+session.headers.update(headers)
+
+try:
+  response = session.get(url, params=parameters)
+  data = json.loads(response.text)
+  print(data)
+except (ConnectionError, Timeout, TooManyRedirects) as e:
+  print(e) # 
